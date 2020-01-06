f16a7572e86edd9d3fd750ae0e0494b88b10a18a
alkamil-junaid <junaid@akbstore.com>
2020-01-06 10:23:20 +0400

    testing repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { PhonegapLocalNotification } from '@ionic-native/phonegap-local-notification/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +20,9 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    LocalNotifications,
+    PhonegapLocalNotification,
+    SocialSharing,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { PhonegapLocalNotification } from '@ionic-native/phonegap-local-notification/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  x = 60;
+  y = 0;
+  title = 'Drag Me!';
 
+  startX = 0;
+  startY = 0;
+
+  constructor(private localNotifications: LocalNotifications, private localNotification: PhonegapLocalNotification, private socialSharing: SocialSharing) {}
+
+  ngOnInit(){
+    // Schedule delayed notification
+    // this.localNotifications.schedule({
+    //   title: 'Local ILocalNotification Example',
+    //   text: 'testing',
+    //   trigger: {at: new Date(new Date().getTime() + 3600)},
+    //   led: 'FF0000',
+    //   sound: null,
+    //   foreground: true
+    // });
+
+    this.localNotification.requestPermission().then(
+      (permission) => {
+        if (permission === 'granted') {
+    
+          // Create the notification
+          this.localNotification.create('My Title', {
+            tag: 'message1',
+            body: 'My body\nNew line\n2nd Line',
+            icon: 'assets/icon/favicon.ico',
+            
+          });
+    
+        }
+      }
+    );
+
+    // Check if sharing via email is supported
+this.socialSharing.canShareViaEmail().then(() => {
+  // Sharing via email is possible
+}).catch(() => {
+  // Sharing via email is not possible
+});
+
+// Share via email
+this.socialSharing.share('Body', 'Subject', ['recipient@example.org']).then(() => {
+  // Success!
+}).catch(() => {
+  // Error!
+});
+
+  }
+
+  onPress(event){
+    console.log(event);
+  }
+
+  onPan(event: any): void {
+    event.preventDefault();
+    if(event.additionalEvent === 'pandown'){
+      if(this.x > 60)
+         this.x = this.x - 2.5;
+    }
+    else if(event.additionalEvent === 'panup'){
+      if(this.x < 300)
+         this.x = this.x + 2.5;
+    }
+  }
 }
